c5861596b78380a2bfb30994a82f8ec76f8bc1da
jkhoang313 <jkhoang313@gmail.com>
2021-01-25 19:19:36 -0500

    Include withDragAndDrop in build command (#69)
    Co-authored-by: Jackson Hoang <jhoang@atlassian.com>

--- examples/demos/dnd.js
+++ examples/demos/dnd.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import events from '../events'
-import { Calendar, Views } from 'react-big-calendar'
-import withDragAndDrop from 'react-big-calendar/lib/addons/dragAndDrop'
+import { Calendar, Views, withDragAndDrop } from 'react-big-calendar'
 
 import 'react-big-calendar/lib/addons/dragAndDrop/styles.scss'
 
